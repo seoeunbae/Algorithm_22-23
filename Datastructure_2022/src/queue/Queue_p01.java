be48b7493a28aa85d34bba391f67a291250bd52b
seoeunbae <enyoupark@gmail.com>
2022-05-10 17:46:30 +0900

    queue-selfimplementation

--- Datastructure_2022/src/queue/Queue_p01.java
+++ Datastructure_2022/src/queue/Queue_p01.java
@@ -1,4 +1,43 @@
 package queue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue_p01 {
+    public static List enqueue(Integer number, List queue){
+
+        if( queue.isEmpty()){
+            queue.add(number);
+        } else {
+            queue.add(0, number);
+        }
+        return queue;
+    }
+
+    public static List dequeue(List queue){
+        int length = queue.size();
+        queue.remove(length-1);
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> queue = new ArrayList();
+
+        enqueue(1,queue);
+        System.out.println(queue);
+        enqueue(2,queue);
+        System.out.println(queue);
+        enqueue(3,queue);
+        System.out.println(queue);
+        enqueue(4,queue);
+        System.out.println(queue);
+        dequeue(queue);
+        System.out.println(queue);
+        dequeue(queue);
+        System.out.println(queue);
+        dequeue(queue);
+        System.out.println(queue);
+        dequeue(queue);
+        System.out.println(queue);
+    }
 }
