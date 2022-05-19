5fa8bb79a9de5283bd7e6c9c03b0753ceae58023
seoeunbae <enyoupark@gmail.com>
2022-05-19 09:12:31 +0900

    sort-insertionsort-self

--- Algorithm/src/sort/InsertionSort01.java
+++ Algorithm/src/sort/InsertionSort01.java
@@ -1,4 +1,18 @@
 package sort;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class InsertionSort01 {
+    //두 개일때 동작
+    List<Integer> dataList = new ArrayList<>();
+
+    public void sort(List<Integer> dataList){
+        if( dataList.get(0)>dataList.get(1)){
+            Collections.swap(dataList, 0 , 1);
+        }
+        System.out.println(dataList);
+    }
 }
