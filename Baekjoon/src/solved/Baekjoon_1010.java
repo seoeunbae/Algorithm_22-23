6ce105eb0ce118a0e616ed59cdd46572eb4c23c3
seoeunbae <enyoupark@gmail.com>
2022-11-09 17:50:25 +0900

    Baekjoon-조합 관련 기출 문제

---
+++ Baekjoon/src/solved/Baekjoon_1010.java
@@ -0,0 +1,33 @@
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1010 {
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer T = Integer.valueOf(br.readLine());
+
+        for(int i=0 ; i < T ;i++){
+            String[] inputs = br.readLine().split(" ");
+            int N = Integer.parseInt(inputs[0]);
+            int M = Integer.parseInt(inputs[1]);
+            dp = new int[M+1][N+1];
+            System.out.println(dpFunc(M,N));
+        }
+    }
+
+    public static Integer dpFunc(int n, int k){ //n개 중에 k개
+        if(dp[n][k] > 0){
+            return dp[n][k];
+        }
+
+        if(n==k || k==0){
+            return dp[n][k]=1;
+        }
+
+        return dp[n][k]= dpFunc(n-1,k-1)+dpFunc(n-1,k);
+    }
+}
---
+++ Baekjoon/src/solved/Baekjoon_1149.java
@@ -0,0 +1,42 @@
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Baekjoon_1149 {
+    final static int RED = 0;
+    final static int GREEN = 1;
+    final static int BLUE = 2;
+
+    static int[][] cost;
+//    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+
+        cost = new int[N][3];
+
+        //cost 초기화
+        for(int i=0 ; i<N ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i][RED] = Integer.parseInt(st.nextToken());
+            cost[i][GREEN] = Integer.parseInt(st.nextToken());
+            cost[i][BLUE] = Integer.parseInt(st.nextToken());
+        }
+        //메모이제이션, top-down 방식
+        for(int i=1; i< N ; i++){
+            cost[i][RED] += Math.min(cost[i-1][GREEN], cost[i-1][BLUE]);
+            cost[i][GREEN] += Math.min(cost[i-1][RED], cost[i-1][BLUE]);
+            cost[i][BLUE] += Math.min(cost[i-1][GREEN], cost[i-1][RED]);
+        }
+
+
+        System.out.println(Math.min(Math.min(cost[N-1][RED], cost[N-1][GREEN]), cost[N-1][BLUE]));
+    }
+
+
+
+}
