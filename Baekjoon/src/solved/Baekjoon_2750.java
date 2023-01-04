c29eb193b0a9c9e5d6403afa8249f5b42df6bb2e
seoeunbae <enyoupark@gmail.com>
2023-01-04 11:25:28 +0900

    baekjoon-수정렬-PriorityQueue의 Comparator인터페이스 오버라이드

--- Baekjoon/src/solved/Baekjoon_2750.java
+++ Baekjoon/src/solved/Baekjoon_2750.java
@@ -1,20 +1,30 @@
-//package solved;
-//
-//import java.util.Scanner;
-//
-//public class Baekjoon_2750 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        scanner.next
-//        int[] arr;
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            for (int j = i + 1; j < arr.length; j++) {
-//                if (arr[i] > arr[j]) {
-//                    int temp = arr[j];
-//                    arr[j] = arr[i];
-//                    arr[i] = temp;
-//                }
-//            }
-//        }
-//    }
-//}
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Baekjoon_2750 {
+    static PriorityQueue<Integer> sorting = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1-o2;
+        }
+    });
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(br.readLine());
+        for(int i=0 ; i<n ; i++){
+            sorting.add(Integer.parseInt(br.readLine()));
+        }
+        printAnswer();
+    }
+
+    public static void printAnswer(){
+        while(!sorting.isEmpty()){
+            System.out.println(sorting.poll());
+        }
+    }
+}
