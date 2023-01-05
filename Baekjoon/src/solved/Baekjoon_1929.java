a3d397522a896f07fbcd6ead691bc560ffdb3e55
seoeunbae <enyoupark@gmail.com>
2023-01-05 14:37:52 +0900

    baekjoon-에라토스테네스의 체활용하기

---
+++ Baekjoon/src/solved/Baekjoon_1929.java
@@ -0,0 +1,32 @@
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1929 {
+    static int[] primeArray;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int M = Integer.parseInt(split[0]);
+        int N = Integer.parseInt(split[1]);
+        primeArray = new int[N+1];
+        for(int i=1; i<=N ; i++){
+            primeArray[i] = i;
+        }
+        for(int i=2 ; i<=Math.sqrt(N) ; i++){
+            //이미 지워진 애는 건너뛰기
+            if(primeArray[i] == 0) continue;
+
+            for(int j=2*i ; j<=N ; j+=i){
+                //배수인 애 지우기
+                primeArray[j] = 0;
+            }
+        }
+
+        for(int i=M; i<=N;i++){
+            if(primeArray[i] != 0) System.out.println(primeArray[i]);
+        }
+    }
+}
