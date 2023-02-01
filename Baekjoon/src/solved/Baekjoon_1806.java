75cc01aefe3e5c074ee421aa0e5c1b953b37a4e4
seoeunbae <enyoupark@gmail.com>
2023-02-01 13:43:09 +0900

    baekjoon-투포인터

---
+++ Baekjoon/src/solved/Baekjoon_1806.java
@@ -0,0 +1,46 @@
+package solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon_1806 {
+    static int R = 0;
+    static int answer ;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split =  br.readLine().split(" ");
+
+        Integer N = Integer.parseInt(split[0]);
+        Integer S = Integer.parseInt(split[1]);
+
+        answer = N+2;
+
+        String[] split1 = br.readLine().split(" ");
+        int[] Ns = Arrays.stream(split1).mapToInt(Integer::parseInt).toArray();
+        int sum = 0;
+
+        for(int L = 1  ; L <= N ; L++){
+
+            while( R + 1 <= N && sum < S){
+                R++;
+                sum += Ns[R-1];
+            }
+            if(sum >= S){
+                sum -= Ns[L - 1];
+
+//                System.out.println("ekq");
+                answer = Math.min(answer, R- L + 1);
+            }
+        }
+        if(answer == N + 2){
+//            System.out.println("vdvd");
+            answer = 0;
+        }
+
+        System.out.println(answer);
+        }
+
+}
