de02428a2c8f831e29f7314b4205940b63ec2e1b
seoeunbae <enyoupark@gmail.com>
2023-03-14 16:58:53 +0900

    thisiscote - greedy01

--- Algorithm/src/graph/BFS.java
+++ Algorithm/src/graph/BFS.java
@@ -1,8 +1,6 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class BFS {//2개의 큐(방문된 노드, 탐색이 필요한 노드)
     public static void main(String[] args) {
@@ -25,9 +23,13 @@ public class BFS {//2개의 큐(방문된 노드, 탐색이 필요한 노드)
     public static void BFSFunc(String first, HashMap<String, ArrayList<String>> graph){
         ArrayList<String> visited = new ArrayList<>();
         ArrayList<String> need_visit = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.poll();
         need_visit.add(first);
         int count = 0;
         while(need_visit.size()>0){
+
             count +=1;
             if(visited.contains(need_visit.get(0))){
                 need_visit.remove(0);
@@ -42,3 +44,79 @@ public class BFS {//2개의 큐(방문된 노드, 탐색이 필요한 노드)
     }
 }
 //시간복잡도: O(N+V) 노드수+간선수
+
+
+
+
+class Solution {
+    static Queue<int[]> queue = new LinkedList<>();
+    static long[] result;
+    int[][] board;
+    boolean[][] visited;
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    public long[] solution(int n, int m, int k, int[][] acts) {
+
+        result = new long[k]; //-1해주기
+        board = new int[n+1][m+1];
+        visited = new boolean[n+1][m+1];
+        for(int i=0 ; i < acts.length ; i++){
+            int type = acts[i][0]; //[t, x, p]
+            int x = acts[i][1];
+            int p = acts[i][2];
+            if(type == 1){ //행
+                for(int j=1 ; j<=m ; j++){
+                    board[x][j] = p;
+                    // result[p-1]++;
+                }
+
+            } else { //2//열-row
+                for(int j=1 ; j<=n ; j++){
+                    board[j][x] = p;
+
+                    // result[p-1]++;
+                }
+            }
+        }
+
+        // for(int[] row : board){
+        //     for(int each : row){
+        //         if(each==0) continue;
+        //         result[each-1]++;
+        //     }
+        // }
+        bfs(1,1, n, m);
+        return result;
+
+
+    }
+
+    public void bfs(int x, int y, int n, int m){
+
+
+        queue.add(new int[]{x,y});
+        visited[x][y]=true;
+        while(!queue.isEmpty()){
+            int[] f =queue.poll();
+            int fx = f[0];
+            int fy = f[1];
+            if(board[fx][fy] >= 1) result[board[fx][fy]-1]++;
+
+            for(int i=0; i<4 ; i++){
+                int ax = fx+dx[i];
+                int ay = fy+dy[i];
+                if(ax >= 1 && ax <=n && ay >=1 && ay <=m ){
+                    if(!visited[ax][ay]){
+                        // bfs(ax,ay,n,m);
+                        queue.add(new int[]{ax, ay});
+                        visited[ax][ay] = true;
+
+                    }
+                }
+
+
+            }
+        }
+
+    }
+}
