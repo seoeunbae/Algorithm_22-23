b2c9e7acb546554d162ae4d3a54a005243cfe4a2
seoeunbae <enyoupark@gmail.com>
2023-04-05 12:34:17 +0900

    dp-03

--- Algorithm/src/dynamicprogramming/GuitarList.java
+++ Algorithm/src/dynamicprogramming/GuitarList.java
@@ -1,69 +1,70 @@
 package dynamicprogramming;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class GuitarList {
     static int[] V;
     static int[][] dp;
+    static int[] arr;
+
 
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         int S = sc.nextInt();
         int M = sc.nextInt();
 
 
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
         dp = new int[N][2]; //0...n-1
         V = new int[N];//
+
+        arr = new int[M+1];//
+
+        for(int i=0 ; i <= M ; i++){
+            arr[i] = -1;
+        }
+
+        arr[S] = 0;
 
 
         for(int i=0 ; i < N ; i++){
             V[i] = sc.nextInt(); //V=[5,3,7]
         }
 
-        dp[0][0] = S + V[0];
-        dp[0][1] = S - V[0]; //
+        for(int i=1; i<=N ; i++){
+            int v = V[i-1];
+            List<Integer> list = new ArrayList<>();
 
-        if((dp[0][0] < 0 && dp[0][1] < 0) || ( dp[0][1] >M && dp[0][0] > M) ){
-            System.out.println(-1);
-            return;
+            for(int j=0 ; j <= M ; j++){
+                if(arr[j] == i - 1){
+                    int res1 = j+v;
+                    int res2 = j-v;
+
+                    if( 0 <= res1 && res1 <= M) list.add(res1);
+                    if( 0 <= res2 && res2 <= M) list.add(res2);
+                }
+            }
+
+            for(int n : list){
+                arr[n] = i;
+            }
         }
 
-        for(int i=1; i < N ; i++){
+        int max = -1;
 
-            int zeroPlus = dp[i-1][0] + V[i];
-            int onePlus = dp[i-1][1] + V[i];
-            if(dp[i-1][0] + V[i] > M){
-                zeroPlus = -1;
-            }
-            if(dp[i-1][1] + V[i] > M){
-                onePlus = -1;
-            }
-            if(zeroPlus ==-1 && onePlus== -1){
-                System.out.println(-1);
-                return;
-            }
-            dp[i][0] = Math.max(zeroPlus , onePlus);
 
-            if(dp[i-1][0] - V[i] < 0 && dp[i-1][1] - V[i] < 0){
-                System.out.println(-1);
-                return;
+        for(int i=0; i <= M ; i++) {
+            if(arr[i] == N){
+                max = Math.max(max, i);
             }
-            dp[i][1] = Math.max(dp[i-1][0] - V[i] , dp[i-1][1] - V[i]);
-
         }
-
-
-        System.out.println(Math.max(dp[N-1][0], dp[N-1][1]));
-
-
+        System.out.println(max);
 
 
 
