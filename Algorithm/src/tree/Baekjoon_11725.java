80fe1751b3b1b778b7c234d59a54e8aa5f47e310
seoeunbae <enyoupark@gmail.com>
2023-05-02 20:04:19 +0900

    programmer - dividePlace- unsolved

---
+++ Algorithm/src/tree/Baekjoon_11725.java
@@ -0,0 +1,16 @@
+package tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_11725 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+        for(int i=0 ; i <= N ; i++){
+            String[] s = br.readLine().split(" ");
+            int a =
+        }
+    }
+}
