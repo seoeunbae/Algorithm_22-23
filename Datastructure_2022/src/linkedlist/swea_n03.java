4b5cc30f9ac2ca970376774d8c355f857bb592ac
seoeunbae <baeseoeun1221@gmail.com>
2023-11-03 08:04:23 +0900

    code(datstructure) : linkedlist-swea:

--- Datastructure_2022/src/linkedlist/swea_n03.java
+++ Datastructure_2022/src/linkedlist/swea_n03.java
@@ -1,14 +1,46 @@
 package linkedlist;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class swea_n03 {
     static BufferedReader in;
     static StringTokenizer st;
+    static int[] array;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        array = new int[N];
+        st = new StringTokenizer(st.nextToken());
+        for(int i=0 ; i < N ; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        int cnt =Integer.parseInt(in.readLine());
+        st = new StringTokenizer(in.readLine());
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=0 ; i < cnt ; i++){
+            boolean start = true;
+            if(start){
+                String cmd = st.nextToken();
+                if(cmd.equals("I")){
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+                    for(int z=0 ; z < y ; z++){
+                        int s = Integer.parseInt(st.nextToken());
+                        list.insert(x++,s);
+                    }
+                }else if(cmd.equals("D")){
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+                }else{
+                    int y = Integer.parseInt(st.nextToken());
+                    int s = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
     }
 
     public static class Node<T>{
@@ -36,6 +68,16 @@ public class swea_n03 {
                 node.next = new Node<>(value);
                 tail=node.next;
             }
+        }
+
+        public void insert(int x , T value){
+            Node<T> node = this.head;
+            while (node.next != null && x > 0) {
+                node = node.next;
+                x--;
+            }
+            node.next.next = node.next;
+            node.next = new Node<>(value);
         }
 
     }
